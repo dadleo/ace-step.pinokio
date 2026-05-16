f7e1364339f50fc42d50d11fa425dc40aa7eb533
dadleo <48639643+dadleo@users.noreply.github.com>
2026-05-16 17:27:48 +0800

    Update command in start.js for new model parameters

--- start.js
+++ start.js
@@ -7,7 +7,7 @@ module.exports = {
         env: { },
         path: "app",
         message: [
-          "uv run acestep --port {{port}}{{platform === 'darwin' ? ' --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-1.7B' : ''}}"
+          "uv run acestep --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --offload_threshold 10 {{platform === 'darwin' ? ' --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B' : ''}}"
         ],
         on: [{
           event: "/(http:\/\/[0-9.:]+)/",
