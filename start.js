a15675b544ac1b4338e0a77d0d13c4c2ddd499dd
dadleo <48639643+dadleo@users.noreply.github.com>
2026-05-16 21:46:43 +0800

    Update shell command to bypass invalid peer certificate

--- start.js
+++ start.js
@@ -4,21 +4,15 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        env: { },
         path: "app",
         message: [
-          "uv run acestep --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
+          // The --no-sync flag is the key to bypassing the "invalid peer certificate" errors
+          "uv run --no-sync acestep --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
         ],
         on: [{
           event: "/(http:\/\/[0-9.:]+)/",
           done: true
         }]
-      }
-    },
-    {
-      method: "local.set",
-      params: {
-        url: "{{input.event[1]}}"
       }
     }
   ]
