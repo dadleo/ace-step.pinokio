3e46e70e63909b0c3188f96c35c7d2391a96934d
dadleo <48639643+dadleo@users.noreply.github.com>
2026-05-17 10:47:15 +0800

    Update command to run acestep with Python module

--- start.js
+++ start.js
@@ -9,7 +9,7 @@ module.exports = {
           // --no-sync: Bypasses the SSL/Linux wheel errors
           // --precision bf16: Forces the high-fidelity math the SFT model needs
           // --offload_threshold 15: Locks the 4B Planner in VRAM (Fixes alignment)
-          "uv run --no-sync acestep --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
+          "uv run --no-sync python -m acestep.acestep_v15_pipeline --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
         ],
         on: [{
           event: "/(http:\/\/[0-9.:]+)/",
