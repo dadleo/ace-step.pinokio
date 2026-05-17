cf9731f07dde186f7372178754f6e5c3c78304a6
dadleo <48639643+dadleo@users.noreply.github.com>
2026-05-17 10:49:39 +0800

    Update shell command and event regex in start.js

--- start.js
+++ start.js
@@ -6,13 +6,11 @@ module.exports = {
       params: {
         path: "app",
         message: [
-          // --no-sync: Bypasses the SSL/Linux wheel errors
-          // --precision bf16: Forces the high-fidelity math the SFT model needs
-          // --offload_threshold 15: Locks the 4B Planner in VRAM (Fixes alignment)
+          // The --no-sync flag is mandatory so Pinokio doesn't overwrite your custom Torch 2.7.1 environment
           "uv run --no-sync python -m acestep.acestep_v15_pipeline --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
         ],
         on: [{
-          event: "/(http:\/\/[0-9.:]+)/",
+          event: "/(http:\\/\\/[0-9.:]+)/",
           done: true
         }]
       }
