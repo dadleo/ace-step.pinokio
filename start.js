47742408177868f546c886f49d93686cc7d16f1c
dadleo <48639643+dadleo@users.noreply.github.com>
2026-05-17 11:05:40 +0800

    Update command to use CUDA and flash attention

--- start.js
+++ start.js
@@ -6,8 +6,7 @@ module.exports = {
       params: {
         path: "app",
         message: [
-          // The --no-sync flag is mandatory so Pinokio doesn't overwrite your custom Torch 2.7.1 environment
-          "uv run --no-sync python -m acestep.acestep_v15_pipeline --port {{port}} --init_service true --init_llm true --backend pt --lm_model_path acestep-5Hz-lm-4B --precision bf16 --offload_threshold 15"
+          "uv run --no-sync python -m acestep.acestep_v15_pipeline --port {{port}} --init_service true --init_llm true --backend pt --device cuda --use_flash_attention true"
         ],
         on: [{
           event: "/(http:\\/\\/[0-9.:]+)/",
